fd09aa064d2ea508e2afadc480ca3a01ab76165e
RZajacc <rf.zajac@gmail.com>
2024-10-08 16:12:44 +0200

    created a searchbar and added it to the navigation bar

--- client/src/_components/navigation/NavbarMain.tsx
+++ client/src/_components/navigation/NavbarMain.tsx
@@ -1,24 +1,28 @@
 import Link from "next/link";
+import SearchBar from "../ui/SearchBar";
 
 function NavbarMain() {
   return (
     <>
-      <nav className="flex py-4  justify-between">
+      <nav className="flex py-4 justify-between items-center">
         <section>
-          <Link href={"/"} className="font-bold text-xl ml-7">
+          <Link
+            href={"/"}
+            className="text-xl ml-7 text-emerald-500 font-serif italic tracking-wider"
+          >
             EcoSphere
           </Link>
         </section>
         <section>
-          <button>Search bar</button>
+          <SearchBar />
         </section>
         <section className="mr-7 space-x-4">
-          <Link href={"/login"} className="hover:text-teal-700 font-semibold">
+          <Link href={"/login"} className="hover:text-teal-600 font-semibold">
             Login
           </Link>
           <Link
             href={"/signup"}
-            className="bg-teal-600 px-6 py-[5px] rounded-lg font-semibold text-white hover:bg-teal-700"
+            className="transition-colors delay-150 bg-teal-600 px-6 py-[5px] rounded-lg font-semibold text-white hover:bg-teal-700 duration-500"
           >
             Signup
           </Link>
---
+++ client/src/_components/ui/SearchBar.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import magnifyingGlass from "../../assets/magnifying-glass-backup-svgrepo-com.svg";
+
+function SearchBar() {
+  return (
+    <div className="border border-zinc-400  flex rounded-lg focus-within:border-zinc-900">
+      <input
+        type="text"
+        placeholder="Search by city"
+        className="outline-none pl-2 rounded-l-lg"
+      />
+      <button className="bg-rose-500 p-1 rounded-r-lg">
+        <Image src={magnifyingGlass} alt="Magnifying glass image" width={25} />
+      </button>
+    </div>
+  );
+}
+
+export default SearchBar;
