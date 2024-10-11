6054134d4ce63894a0d02a2447c13864617aa65a
RZajacc <rf.zajac@gmail.com>
2024-10-11 13:45:39 +0200

    fixed a small but with a backrop occupying the screen even though its z-index was negative

--- client/src/_components/navigation/Footer.tsx
+++ client/src/_components/navigation/Footer.tsx
@@ -1,5 +1,30 @@
 function Footer() {
-  return <div className="bg-zinc-600">Footer</div>;
+  return (
+    <div className="bg-zinc-600">
+      <div className="flex justify-around">
+        <section>
+          <p>Your account</p>
+          <ul>
+            <li>Signup</li>
+            <li>Login</li>
+          </ul>
+        </section>
+        <section>
+          <p>Discover</p>
+          <ul>
+            <li>Events</li>
+          </ul>
+        </section>
+        <section>
+          <p>Ecosphere</p>
+          <ul>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- client/src/_components/navigation/SideBar.tsx
+++ client/src/_components/navigation/SideBar.tsx
@@ -11,17 +11,18 @@ type Props = {
 function SideBar({ sideBarActive, setSideBarActive }: Props) {
   // Classes to manage display and transitions of sidebar and backdrop
   const sideBarShowClass =
-    "fixed -left-1/2 top-0 h-full w-1/2 translate-x-full bg-white duration-700 ease-out z-20";
+    "fixed -left-1/2 top-0 z-20 h-full w-1/2 translate-x-full bg-white duration-700 ease-out";
   const sideBarHideClass =
-    "fixed -left-1/2 top-0 h-full w-1/2 -translate-x-full bg-white duration-700 ease-in z-20";
+    "fixed -left-1/2 top-0 h-full w-1/2 -translate-x-full bg-white duration-700 ease-in";
   const backdropActiveClass =
-    "fixed left-0 top-0 z-10 h-full w-full bg-zinc-500 opacity-50 transition-opacity duration-1000";
+    "fixed left-0 top-0 z-10 h-full w-full bg-zinc-500 opacity-50 transition-opacity duration-1000 pointer-events-auto";
   const backdrophidden =
-    "fixed left-0 top-0 -z-10 h-full w-full bg-zinc-500 opacity-0 transition-opacity duration-1000";
+    "fixed left-0 top-0 -z-10 h-full w-full bg-zinc-500 opacity-0 transition-opacity duration-1000 pointer-events-none";
   return (
     <>
       <div className={sideBarActive ? sideBarShowClass : sideBarHideClass}>
-        <div className="relative grid h-1/2 content-center justify-center">
+        {/* <div className="fixed -left-1/2 top-0 z-20 h-full w-1/2 translate-x-full bg-white duration-700 ease-out"> */}
+        <div className="relative z-20 grid h-1/2 content-center justify-center">
           {/* Close sidebar button */}
           <button
             className="absolute right-2 top-2 animate-pulse"
