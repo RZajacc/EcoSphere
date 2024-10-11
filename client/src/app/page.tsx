3f083dc5184f246621852c91d3a52f94afbc7ec3
RZajacc <rf.zajac@gmail.com>
2024-10-11 16:47:58 +0200

    on client side in main page fetched the event data

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
+type eventType = {
+  event_id: number;
+  title: string;
+  user_id: number;
+  description: string;
+  date: string;
+  adress: string;
+};
+
+export default async function Home() {
+  const response = await fetch("http://localhost:5000/events/getAllEvents", {
+    method: "GET",
+    redirect: "follow",
+  });
+  if (!response.ok) {
+    throw Error("Something went wrong");
+  }
+
+  const eventData: { result: eventType[] } = await response.json();
   return (
     <div>
       <h1>Ecosphere home</h1>
+      <ul>
+        {eventData &&
+          eventData.result.map((event) => {
+            return <li key={event.event_id}>{event.title}</li>;
+          })}
+      </ul>
     </div>
   );
 }
