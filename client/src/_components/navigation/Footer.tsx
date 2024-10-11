8f305bad839e21fd8f2d3fcee49cbb58ae2b840b
RZajacc <rf.zajac@gmail.com>
2024-10-11 14:08:01 +0200

    completed simple footer and added 2 empty pages - about and contact and linked them with a footer

--- client/src/_components/navigation/Footer.tsx
+++ client/src/_components/navigation/Footer.tsx
@@ -1,25 +1,62 @@
+import Link from "next/link";
+
 function Footer() {
   return (
-    <div className="bg-zinc-600">
+    <div className="bg-zinc-700 py-1">
       <div className="flex justify-around">
         <section>
-          <p>Your account</p>
-          <ul>
-            <li>Signup</li>
-            <li>Login</li>
+          <p className="font-semibold text-stone-50">Your Account</p>
+          <ul className="ml-1 space-y-1 text-sm text-stone-300">
+            <li>
+              <Link
+                href={"/signup"}
+                className="transition duration-1000 hover:text-yellow-300"
+              >
+                Signup
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/login"}
+                className="transition duration-1000 hover:text-yellow-300"
+              >
+                Login
+              </Link>
+            </li>
           </ul>
         </section>
         <section>
-          <p>Discover</p>
-          <ul>
-            <li>Events</li>
+          <p className="font-semibold text-stone-50">Discover</p>
+          <ul className="ml-1 space-y-1 text-sm text-stone-300">
+            <li>
+              <Link
+                href={"/"}
+                className="transition duration-1000 hover:text-yellow-300"
+              >
+                Events
+              </Link>
+            </li>
           </ul>
         </section>
         <section>
-          <p>Ecosphere</p>
-          <ul>
-            <li>About</li>
-            <li>Contact</li>
+          <p className="font-semibold text-stone-50">Ecosphere</p>
+          <ul className="ml-1 space-y-1 text-sm text-stone-300">
+            <li>
+              <Link
+                href={"/about"}
+                className="transition duration-1000 hover:text-yellow-300"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/contact"}
+                className="transition duration-1000 hover:text-yellow-300"
+              >
+                Contact
+              </Link>
+            </li>
           </ul>
         </section>
       </div>
