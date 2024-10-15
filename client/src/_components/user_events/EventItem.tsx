94606476861e0f3d6620f8a2a12b8f43b10a1f21
RZajacc <rf.zajac@gmail.com>
2024-10-15 17:20:26 +0200

    prepared framework for css grid to display events

--- client/src/_components/user_events/EventItem.tsx
+++ client/src/_components/user_events/EventItem.tsx
@@ -14,14 +14,13 @@ function EventItem({ imageUrl, date, title, adress }: Props) {
   const dateToDisplay = DateFormatter(date);
 
   return (
-    <div>
-      <section>
+    <div className="event">
+      <section className="event_image">
         <Image src={imageUrl} alt={title} width={100} height={100} />
       </section>
-      <p>{dateToDisplay}</p>
-
-      <h1>{title}</h1>
-      <p>{adress}</p>
+      <p className="event_date">{dateToDisplay}</p>
+      <h1 className="event_title">{title}</h1>
+      <p className="event_adress">{adress}</p>
     </div>
   );
 }
