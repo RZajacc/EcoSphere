53a76be29c88617fb6e5bdb79f655afddd68a5f4
RZajacc <rf.zajac@gmail.com>
2024-10-29 14:29:19 +0100

    applied signup logic with redirection to login

--- client/lib/actions/signup.tsx
+++ client/lib/actions/signup.tsx
@@ -1,4 +1,6 @@
+import { error } from "console";
 import { SignupFormSchema, FormState } from "../definitions/SignupFormSchema";
+import { redirect } from "next/navigation";
 
 export async function signup(state: FormState, formData: FormData) {
   // Validate form fields
@@ -14,5 +16,30 @@ export async function signup(state: FormState, formData: FormData) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
     };
+  }
+
+  // Signup logic
+  const { name, email, password } = validatedFields.data;
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+  const urlencoded = new URLSearchParams();
+  urlencoded.append("userName", name);
+  urlencoded.append("email", email);
+  urlencoded.append("password", password);
+
+  const response = await fetch("http://localhost:5000/users/signup", {
+    method: "PUT",
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: "follow",
+  });
+
+  if (!response.ok) {
+    const responsData: { msg: string } = await response.json();
+    throw new Error(responsData.msg);
+  } else {
+    redirect("/login");
   }
 }
