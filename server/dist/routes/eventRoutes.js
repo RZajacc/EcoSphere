304d3acf6b2750f09eca3e42e8e5f94cd2a65cc6
RZajacc <rf.zajac@gmail.com>
2024-11-01 14:28:35 +0100

    removed dist from tracking by git

--- server/dist/routes/eventRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const eventController_1 = require("../controller/eventController");
-const router = (0, express_1.Router)();
-router.post("/getAllByDate", eventController_1.getAllEventsByDate);
-router.post("/getEventByTitle", eventController_1.getEventByTitle);
-exports.default = router;
