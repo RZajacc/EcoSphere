ba584d489a8e5270d543358b857c66139de462e5
RZajacc <rf.zajac@gmail.com>
2024-11-04 16:17:41 +0100

    in desktop nav added user image as a button responding to click

--- client/src/_components/navigation/NavDesktop.tsx
+++ client/src/_components/navigation/NavDesktop.tsx
@@ -1,7 +1,16 @@
+"use client";
 import Link from "next/link";
 import SearchBar from "@/_components/ui/navigation/SearchBar";
+import { useContext, useState } from "react";
+import { AuthContext } from "@/context/AuthContext";
+import noUserImg from "../../assets/noUser.png";
+import pointerUp from "../../assets/pointerup.svg";
+import pointerDown from "../../assets/pointerdown.svg";
+import Image from "next/image";
 
 function NavDesktop() {
+  const { user } = useContext(AuthContext);
+  const [showDropdown, setShowDropdown] = useState(false);
   return (
     <div className="hidden items-center justify-between py-4 sm:flex">
       <section>
@@ -15,17 +24,40 @@ function NavDesktop() {
       <section>
         <SearchBar />
       </section>
-      <section className="mr-7 space-x-4">
-        <Link href={"/login"} className="font-semibold hover:text-teal-600">
-          Login
-        </Link>
-        <Link
-          href={"/signup"}
-          className="rounded-lg bg-teal-600 px-6 py-[5px] font-semibold text-white transition-colors delay-150 duration-500 hover:bg-teal-700"
-        >
-          Signup
-        </Link>
-      </section>
+      {user ? (
+        <section>
+          <button
+            className="mr-7 flex items-center"
+            onClick={() => {
+              setShowDropdown((prevVal) => !prevVal);
+            }}
+          >
+            <Image
+              src={noUserImg}
+              alt="No user image"
+              width={42}
+              className="rounded-full"
+            />
+            <Image
+              src={showDropdown ? pointerDown : pointerUp}
+              alt="Pointer down"
+              width={25}
+            />
+          </button>
+        </section>
+      ) : (
+        <section className="mr-7 space-x-4">
+          <Link href={"/login"} className="font-semibold hover:text-teal-600">
+            Login
+          </Link>
+          <Link
+            href={"/signup"}
+            className="rounded-lg bg-teal-600 px-6 py-[5px] font-semibold text-white transition-colors delay-150 duration-500 hover:bg-teal-700"
+          >
+            Signup
+          </Link>
+        </section>
+      )}
     </div>
   );
 }
