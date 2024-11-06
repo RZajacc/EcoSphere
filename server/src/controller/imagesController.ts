21c3f805941c765da926129c87eb1cb424328355
RZajacc <rf.zajac@gmail.com>
2024-11-06 15:22:50 +0100

    managed to run authentication for GCP and connect with it in images controller. So far only to list buckets

--- server/src/controller/imagesController.ts
+++ server/src/controller/imagesController.ts
@@ -1,4 +1,17 @@
 import { RequestHandler } from "express";
+import { Storage } from "@google-cloud/storage";
+
+const storage = new Storage({ projectId: process.env.PROJECT_ID });
+
+async function listBuckets() {
+  const [buckets] = await storage.getBuckets();
+  console.log("Buckets:");
+  buckets.forEach((bucket) => {
+    console.log(bucket.name);
+  });
+}
+
+listBuckets().catch(console.error);
 
 const uploadImage: RequestHandler = (req, res) => {
   res.status(200).json({
